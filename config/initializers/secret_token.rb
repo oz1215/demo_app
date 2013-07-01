eda9a68506a322a195b7b70f90ed20c4c114626a
三神 行蔵 <kozo_mikami@dwango.co.jp>
2013-07-01 15:34:54 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '8bac49a8c27e898f73b0513e98393ef2df541dfb038955a9a9afcba6dcf68808ca043069c4dc203a9c149f96f51091c4e8e3dd1f187355ec8860abb89dca4927'
